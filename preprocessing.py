48994717b7c379ab1d32721cef4d3de8b2eacff5
patrick <paatrick.yang880612@gmail.com>
2023-09-18 13:38:05 +0800

    update to taper can run, next instrument reponse

--- preprocessing.py
+++ preprocessing.py
@@ -57,46 +57,48 @@ output_parent_dir = '/raid1/SM_data/archive/2023/TW/preprocessing' # parent fold
 os.makedirs(output_parent_dir, exist_ok=True) # create la
 
 # List of subdirectories within the parent directory.
-subdirectories = ['SM02', 'SM06', 'SM09', 'SM19', 'SM37', 'SM39', 'SM40']
+subdirectories = ['SM01','SM02', 'SM06', 'SM09', 'SM19', 'SM37', 'SM39', 'SM40']
 sub_subdirectories = ['EPZ.D','EPN.D', 'EPE.D']
 # Loop through each subdirectory.
 for subdirectory in subdirectories:
     # Define the full path to the current subdirectory.
-    subdirectory_path = os.path.join(parent_directory, subdirectory)
+    subdirectory_path = os.path.join(parent_directory, subdirectory) # ./TW/SM02
     # I finally understand it. For example, parent_directory='/path/qoo', and subdirectory is 'juice'
     # when we use os.path.join, it will be like /path/qoo/juice, fucking cool.
     
     # Create a corresponding parent subdirectory in the output parent directory with the naming pattern.
-    parent_subdirectory = os.path.join(output_parent_dir, f'{subdirectory}_preprocessing')
+    parent_subdirectory = os.path.join(output_parent_dir, subdirectory) # ./preprocessing/SM02
     os.makedirs(parent_subdirectory, exist_ok=True)
 
-    for sub_subdirectory in sub_subdirectories:
+    for sub_subdirectory in sub_subdirectories: 
         # define the full path to current sub_subdirectory.
-        sub_subdirectory_path = os.path.join(parent_subdirectory, sub_subdirectories)
+        sub_subdirectory_path = os.path.join(subdirectory_path, sub_subdirectory) # ./TW/SM02/EPZ.D
 
         # creating the corresponding output directory
-        output_directory = os.path.join(sub_subdirectory_path)
+        output_directory = os.path.join(parent_subdirectory, sub_subdirectory) # ./preprocessing/SM02/EPZ.D
+        os.makedirs(output_directory, exist_ok=True)
 
     # Use glob to find all files with a specific extension (e.g., .mseed) in the subdirectory.
-    data_files = glob.glob(os.path.join(subdirectory_path, ))
+    # data_files = glob.glob(sub_subdirectory_path) # no need to use glob
+        data_files = os.listdir(sub_subdirectory_path)
 
     # Loop through each data file in the subdirectory and process them.
-    for data_file in data_files:
-        # Read the seismic data file into a Stream object.
-        st = read(data_file)
+        for data_file in data_files:
+            # Read the seismic data file into a Stream object.
+            st = read(data_file)
 
-        # Merge traces within the Stream if there is more than one trace.
-        if len(st) > 1:
-            st = st.merge(method=1, fill_value=0)
+            # Merge traces within the Stream if there is more than one trace.
+            if len(st) > 1:
+                st = st.merge(method=1, fill_value=0)
 
-        # Preprocess the Stream.
-        st.detrend('demean')
-        st.detrend('linear')
-        st.taper(type='hann', max_percentage=0.05)
+            # Preprocess the Stream.
+            st.detrend('demean')
+            st.detrend('linear')
+            st.taper(type='hann', max_percentage=0.05)
 
-        # Define the output file path and save the preprocessed Stream.
-        output_file = os.path.join(output_subdirectory, os.path.basename(data_file))
-        st.write(output_file, format='MSEED')
+            # Define the output file path and save the preprocessed Stream.
+            output_file = os.path.join(output_directory, os.path.basename(data_file))
+            st.write(output_file, format='SAC')
 
 print("Preprocessing and saving completed for all directories.")
 
