2b667c4ba68a2c3fcd68a406b3bc80988b833bce
patrick <paatrick.yang880612@gmail.com>
2023-09-18 14:51:50 +0800

    till the taper

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,3 @@
-# %%
 import obspy
 from obspy import read
 import os
@@ -6,50 +5,6 @@ from obspy.io.sac import attach_paz
 from obspy.signal.invsim import corn_freq_2_paz
 from obspy.signal import PPSD
 import glob
-# follow the filename: 3039.FM.00.DPE.2023.002.20230102.SAC
-#%%
-# merge->demean->detrend->taper
-
-# Define the directory containing your seismic data files (EPZ).
-data_directory = '/raid1/SM_data/archive/2023/TW/SM01/EPZ.D/'
-output_dir = '/raid1/SM_data/archive/2023/TW/SM01_ppreprocessing'
-os.makedirs(output_dir, exist_ok=True)
-# exist_ok, when dir is exist that won't exist error.
-
-# Use glob to find all files with a specific extension (e.g., .mseed) in the directory.
-data_files = glob.glob(os.path.join(data_directory, '*EPZ*'))
-
-# Loop through each data file and process them.
-for data_file in data_files:
-    # Read the seismic data file into a Stream object.
-    st = read(data_file)
-
-    # Merge traces within the Stream if there is more than one trace.
-    if len(st) > 1:
-        st = st.merge(method=1, fill_value=0)
-        # method 1: more flexible, but for our purpose there's no difference between using method 0 or 1.
-        # filled = 0, that's a default, we don't want to interpolate the value.
-    # Preprocess the Stream.
-    st.detrend('demean')
-    st.detrend('linear')
-    st.taper(type='hann', max_percentage=0.05)
-
-    # Define the output file path and save the preprocessed Stream.
-    output_file = os.path.join(output_dir, os.path.basename(data_file))
-    st.write(output_file, format='SAC')
-
-print("Preprocessing and saving completed.")
-#%%
-# Remove Instrument response
-corner_freq = 5
-pre_filt = (0.05, 0.1, 30, 40)
-damping = 0.7
-Pzs_path = '/raid1/SM_data/TWSM2/SM01/SAC_PZs_TW_SM01_EPZ__2021.001.00.00.00.0000_2599.365.23.59.59.99999'
-attach_paz(st[0], Pzs_path)
-paz_1hz = corn_freq_2_paz(corner_freq, damp=damping)
-st.simulate(paz_remove=paz_1hz, pre_filt=pre_filt)
-
-#%%
 
 # Define the parent directory containing subdirectories with seismic data.
 parent_directory = '/raid1/SM_data/archive/2023/TW'
@@ -101,4 +56,3 @@ for subdirectory in subdirectories:
             st.write(output_file, format='SAC')
 
 print("Preprocessing and saving completed for all directories.")
-
