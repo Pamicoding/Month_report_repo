940fa3b82b49eeecf9d8c19a3edffc89e0b12b35
patrick <paatrick.yang880612@gmail.com>
2023-09-20 15:47:25 +0800

    up

--- test_bandpass.py
+++ test_bandpass.py
@@ -1,6 +1,7 @@
 # %%
 # spectrogram test
-from obspy.core import read, UTCDateTime
+from obspy import imaging
+from obspy import read, UTCDateTime
 import os
 import glob
 import matplotlib.pyplot as plt
@@ -12,6 +13,7 @@ st_freq = st_copy.filter("bandpass", freqmin=0.1, freqmax=10)
 st_time = st_freq[0].times() # Times
 st_data = st_freq[0].data # the signal
 
+
 fig = plt.figure(layout= "constrained")
 gs = gridspec.GridSpec(2, 3, figure=fig, height_ratios=[1, 1], width_ratios=[1, 1, 0.05])
 ax1 = plt.subplot(gs[0, :-1])
@@ -21,15 +23,17 @@ cax = plt.subplot(gs[1, -1])
 ax1.plot(st_time, st_data)
 ax1.set_ylabel('Signal')
 
-# spectrogram setting
-Fs = 100
-NFFT = 1024
-ax2.specgram(st_data, NFFT=NFFT, Fs=Fs)
-ax2.set_xlabel('Times')
-ax2.set_ylabel('Frequency (Hz)')
+# spectrogram in ObsPy
+st_freq.spectrogram(log=True, title='SM01'+str(st_freq[0].stats.starttime), axes = ax2)
+# spectrogram setting for matplotlib
+#Fs = 100
+#NFFT = 1024
+#ax2.specgram(st_data, NFFT=NFFT, Fs=Fs)
+#ax2.set_xlabel('Times')
+#ax2.set_ylabel('Frequency (Hz)')
 
 # Create the colorbar for the spectrogram
-cbar = plt.colorbar(ax2.images[0], cax=cax, format='%+3.0f')
+cbar = plt.colorbar(ax2.list[0], cax=cax, format='%+2.0f')
 
 # Set the label for the colorbar
 cbar.set_label('Power/Frequency (dB/Hz)')
