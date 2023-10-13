949c5ea62470d9ae7cbefaa04a1bcf85bc74ee5e
patrick <paatrick.yang880612@gmail.com>
2023-10-13 14:59:51 +0800

    update to latest

--- bandpass_spectrogram.py
+++ bandpass_spectrogram.py
@@ -9,13 +9,13 @@ from matplotlib.ticker import FuncFormatter
 from matplotlib import gridspec
 
 # variable
-starttime_trim = UTCDateTime("2023-09-20T12:31:29") # change it
-endtime_trim = UTCDateTime("2023-09-20T12:41:29") # change it
+starttime_trim = UTCDateTime("2023-09-20T11:04:52") # change it
+endtime_trim = UTCDateTime("2023-09-20T11:14:52") # change it
 parent_dir = '/raid1/SM_data/archive/2023/TW/remove_resp/'
 station_list = os.listdir(parent_dir)
 days = 263 # change it
 my_days = '*263*' # change it
-output_dir = '/home/patrick/Work/Month_report_repo/event_spectrogram'
+output_dir = '/home/patrick/Work/Month_report_repo/output_sep'
 # Create the directory if it doesn't exist
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
@@ -39,7 +39,7 @@ for station in station_list:
         st_copy = st.copy()
         st_copy[0].trim(starttime=starttime_trim, endtime=endtime_trim)
         st_copy.taper(type='hann', max_percentage=0.05)
-        st_freq = st_copy.filter("bandpass", freqmin=0.01, freqmax=45)
+        st_freq = st_copy.filter("bandpass", freqmin=0.1, freqmax=10)
         st_time = st_freq[0].times() # Times
         st_data = st_freq[0].data # the signal
         # set the axes
@@ -72,15 +72,15 @@ for station in station_list:
         ax2.set_ylabel('Frequency (Hz)', fontsize = 12)
         ax2.set_yscale('log')
         ax2.set_xlim(0,600)
-        ax2.set_ylim(0.5, 50)
+        ax2.set_ylim(0.1, 10)
 
         # Customize the y-axis tick labels to be in scientific notation
         ax2.yaxis.set_major_formatter(FuncFormatter(scientific_formatter))
 
         # Display the plot0
-        filename = f"{station}_{days}_signal.png"
+        filename = f"{station}_{days}_signal_a.png"
         file_path = os.path.join(output_dir,filename)
-        plt.savefig(file_path, dpi=300, bbox_inches = 'tight')
+        #plt.savefig(file_path, dpi=300, bbox_inches = 'tight')
     except IndexError:
         print(f"{station} do not have the data")
 
