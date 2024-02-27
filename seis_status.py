e4d44367dbdd185f2b3c14bde833cdbea4044658
Patrick Yang <patrick.yang880612@gmail.com>
2024-02-27 15:17:58 +0800

    modify the grammar in glob

--- seis_status.py
+++ seis_status.py
@@ -3,17 +3,19 @@ import os
 import glob
 from  obspy.imaging.scripts.scan import Scanner
 from obspy import read, UTCDateTime
-
+#%%
 # variable
+output ='/home/patrick/Work/Month_report_repo/output/2023_oct'
+year = '2023'
 month = 'October'
-starttime = UTCDateTime(2023, 10, 1)
-endtime = UTCDateTime(2023, 10, 31)
+starttime = UTCDateTime(year, 10, 1)
+endtime = UTCDateTime(year, 10, 31)
 title = f"{month}_data_availability"
 equip_list = ['EPZ.D','HLZ.D']
 # loop
 for equip in equip_list:
     scanner = Scanner()
-    for i in glob.glob('/raid1/SM_data/archive/2023/TW/remove_resp/SM*/%s%s%s' %('*', equip, '*')):
+    for i in glob.glob(f"/raid1/SM_data/archive/{year}/TW/remove_resp/SM*/{'*'+equip+'*'}"):
         scanner.parse(i)
-    scanner.plot(starttime=starttime, endtime=endtime, outfile=f'{title}_{equip}.png', print_gaps=True)
-# %%
+    scanner.plot(starttime=starttime, endtime=endtime, outfile=f'{output}/{title}_{equip}.png', print_gaps=True)
+#%%
